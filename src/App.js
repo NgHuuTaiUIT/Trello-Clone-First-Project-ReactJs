47cdc09085787fbeb256f75ee17e23ad95e69233
Tài Nguyễn Hữu <18521361@ms.uit.edu.vn>
2021-08-29 21:44:02 +0700

    Design trello Front-end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-       <h1>App</h1>
+       <nav className="navbar app">App Bar</nav>
+       <nav className="navbar board">Borad Bar</nav>
+       <div className="board-columns">
+         <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://f40-zpg.zdn.vn/5907652352288567255/ffd08cfe8bff79a120ee.jpg" alt="" />
+                <br />
+                Title: Mangekyo
+              </li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+            </ul>
+            <footer>Add another card</footer>
+         </div>
+         <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://f40-zpg.zdn.vn/5907652352288567255/ffd08cfe8bff79a120ee.jpg" alt="" />
+                <br />
+                Title: Mangekyo
+              </li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+            </ul>
+            <footer>Add another card</footer>
+         </div>         <div className="column">
+            <header>Brainstorm</header>
+            <ul>
+              <li>
+                <img src="https://f40-zpg.zdn.vn/5907652352288567255/ffd08cfe8bff79a120ee.jpg" alt="" />
+                <br />
+                Title: Mangekyo
+              </li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+              <li>Them 1 cai j ban thich</li>
+            </ul>
+            <footer>Add another card</footer>
+         </div>
+       </div>
     </div>
   );
 }
